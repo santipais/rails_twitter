e233df213aaf4210fd1c48fc669f5cb795b96b25
Santiago Pais <santiago.pais@gogrow.dev>
2024-11-11 15:44:34 -0300

    added rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,9 @@
 
 FactoryBot.define do
   factory :user do
+    username { Faker::Internet.username(specifier: 5..10) }
     email { Faker::Internet.email }
+    password { 'secret' }
+    password_confirmation { 'secret' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { build(:user) }
+
   describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(user).to be_valid
+    end
+
     it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_presence_of(:encrypted_password) }
     it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it { is_expected.to validate_uniqueness_of(:username).ignoring_case_sensitivity }
+    it { is_expected.to validate_length_of(:username).is_at_least(3) }
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+    it { is_expected.to allow_value('test@example.com').for(:email) }
+    it { is_expected.not_to allow_value('invalid_email').for(:email) }
   end
 end
