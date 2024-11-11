72083ef31497371ea7b54f5fc85ec935183de4be
Santiago Pais <santiago.pais@gogrow.dev>
2024-11-11 11:54:03 -0300

    updated signup view
    Also added flash partial

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
 gem 'sprockets-rails'
 gem 'stimulus-rails'
-gem 'tailwindcss-rails'
+gem 'tailwindcss-rails', '~> 2.0', '>= 2.0.32'
 gem 'turbo-rails'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,39 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        dark: {
+          900: "#0F1419",
+          800: "#17202A",
+          700: "#1C2733",
+          600: "#283340",
+          500: "#3A444C",
+          400: "#5B7083",
+          300: "#8899A6",
+          200: "#EBEEF0",
+          100: "#F7F9FA",
+        },
+        primary: "#1DA1F2",
+        "primary-dark": "#1A91DA",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+  safelist: ['field_with_errors']
+};
