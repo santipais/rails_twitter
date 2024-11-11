44aee076e91773f131a4f2489d565ad117869687
Santiago Pais <santiago.pais@gogrow.dev>
2024-11-11 15:04:15 -0300

    added username

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, presence: true, uniqueness: { case_sensitive: true }
+  validates :username, presence: true, length: { minimum: 3 }, uniqueness: { case_sensitive: true }
   validates :encrypted_password, presence: true
 end
