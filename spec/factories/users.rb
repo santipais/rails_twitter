64fc53aa91fcd978291ca8e36242794e1f075a97
Santiago Pais <santiago.pais@gogrow.dev>
2024-11-13 11:32:58 -0300

    removed password confirmation, generate password with Faker in user factorybot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
   factory :user do
     username { Faker::Internet.username(specifier: 5..10) }
     email { Faker::Internet.email }
-    password { 'secret' }
-    password_confirmation { 'secret' }
+    password { Faker::Internet.password(min_length: 6, max_length: 8) }
   end
 end
