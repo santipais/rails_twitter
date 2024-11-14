d1bb49dc01356c71be3b143455a0106557782e54
Santiago Pais <santiago.pais@gogrow.dev>
2024-11-14 15:02:00 -0300

    changed flash partial, deleted session controller.

--- app/controllers/users/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Users
-  class SessionsController < Devise::SessionsController
-    skip_before_action :verify_authenticity_token, with: :null_session
-  end
-end
