3d95cc55452f9d2682963f7864176995ce865d49
Santiago Pais <santiago.pais@gogrow.dev>
2024-12-10 12:28:08 -0300

    Added show to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::Base
   respond_to :html, :json
+
+  private
+
+  def current_user?(user)
+    current_user == user
+  end
+
+  helper_method :current_user?
 end
