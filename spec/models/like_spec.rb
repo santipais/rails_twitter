f72a6308151c3168dd20dba274ecf86020cc7222
Santiago Pais <santiago.pais@gogrow.dev>
2024-12-12 16:41:04 -0300

    fixed pr comments

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -6,26 +6,28 @@ RSpec.describe Like, type: :model do
   subject { build(:like) }
 
   describe 'validations' do
-    let(:user) { create(:user) }
-    let(:tweet) { create(:tweet, user: user) }
+    it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:tweet_id).with_message(I18n.t('errors.likes.already_liked')) }
 
     context 'when user likes a tweet' do
-      let(:like) { build(:like, user: user) }
+      let(:user) { create(:user) }
+
+      subject(:like) { build(:like, user: user) }
 
       it 'is valid' do
         expect(like.valid?).to eq(true)
       end
-    end
-    context 'when user likes their own tweet' do
-      let(:like_own_tweet) { build(:like, user: user, tweet: tweet) }
 
-      it 'is invalid' do
-        expect(like_own_tweet.valid?).to eq(false)
-        expect(like_own_tweet.errors.full_messages).to include("Users #{I18n.t('errors.likes.own_tweet')}")
+      context 'when user likes their own tweet' do
+        let(:tweet) { create(:tweet, user: user) }
+
+        subject(:like) { build(:like, user: user, tweet: tweet) }
+
+        it 'is invalid' do
+          expect(like.valid?).to eq(false)
+          expect(like.errors.full_messages).to include("Users #{I18n.t('errors.likes.own_tweet')}")
+        end
       end
     end
-
-    it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:tweet_id).with_message(I18n.t('errors.likes.already_liked')) }
   end
 
   describe 'associations' do
