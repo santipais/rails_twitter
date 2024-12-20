d616ae93bb87e7e09fd79ba0bb874c7fcdb84750
Santiago Pais <santiago.pais@gogrow.dev>
2024-12-20 15:00:06 -0300

    new feed
    Modified which tweets are displayed on the feed.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,4 +6,12 @@ class Tweet < ApplicationRecord
   has_many :likers, through: :likes, source: :user
 
   validates :content, presence: true, length: { maximum: 280 }
+
+  scope :feed, lambda { |user|
+    return all if user.blank?
+
+    where(id: user.following_users_tweets).or(
+      where(id: user.following_users_likes.select(:tweet_id))
+    ).excluding(user.tweets).distinct
+  }
 end
