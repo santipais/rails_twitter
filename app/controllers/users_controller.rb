98911ff22b201ad4b9868d12a98f1680bdd7abc7
Santiago Pais <santiago.pais@gogrow.dev>
2025-01-07 11:36:47 -0300

    fix pr comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!
 
   def show
-    @user = User.includes(:followers_users).find_by!(username: params[:id])
+    @user = User.includes(:followers_users).with_attached_profile_image.find_by!(username: params[:id])
     @following_user = current_user.present? && @user.followers_users.include?(current_user)
   end
 end
